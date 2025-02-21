56cfba8ccf188e6f11f33f1498ede16c47c967ed
Sagar <sagar.patkar@techchefz.com>
2025-02-22 04:08:18 +0530

    Updated About file

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,13 +4,13 @@ const About = () => {
     return (
         <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About Me">
             <p className="mb-4">
-                I’m a developer passionate about building accessible and scalable web applications. I love working on the development side, creating seamless user experiences that are not only visually appealing but also optimized for performance and usability.
+                I am a developer passionate about building accessible and scalable web applications. I love working on the development side, creating seamless user experiences that are not only visually appealing but also optimized for performance and usability.
             </p>
             <p className="mb-4">
-                Currently, I’m an Associate Technology at Techchefz Pvt. Ltd., where I focus on developing and maintaining UI components, ensuring they meet web accessibility standards and best practices to enhance user experience.
+                Currently, I am an Associate Technology at Techchefz Pvt. Ltd., where I focus on developing and maintaining UI components, ensuring they meet web accessibility standards and best practices to enhance user experience.
             </p>
             <p className="mb-4">
-                Throughout my career, I've had the opportunity to develop features across diverse industries, including insurance and education. Additionally, I’ve completed various courses on Udemy and earned certifications to deepen my expertise.
+                Throughout my career, I have had the opportunity to develop features across diverse industries, including insurance and education. Additionally, I have completed various courses on Udemy and earned certifications to deepen my expertise.
             </p>
             <p className="mb-4">
                 Outside of coding, I enjoy coaching football at a local school, spending time with my wife and dog, or going for a bike ride.
